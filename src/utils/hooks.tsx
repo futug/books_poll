9bfad209eb5774e382eb1cae62eb9b73e7ae68ec
futug <102650930+futug@users.noreply.github.com>
2024-06-12 23:01:17 +0600

    tg button

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 
 type DebouncedValue = {
   debouncedValue: string;
@@ -22,31 +22,31 @@ export function useDebounce(value: string, delay: number): DebouncedValue {
   return { debouncedValue, pending };
 }
 
-export function useFetch (url: string, method: string = 'GET', headers: HeadersInit, body: Record<string, unknown>[] | null = null) {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [pending, setPending] = useState(false);
+// export function useFetch (url: string, method: string = 'GET', headers: HeadersInit, body: Record<string, unknown>[] | null = null) {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [pending, setPending] = useState(false);
 
-  const fetchHandler = useCallback(async () => {
-    setPending(true);
-    try {
-      const res = await fetch(url, { 
-        method,
-        headers,
-       });
-      if(body) {
-        const json = await res.json();
-        setData(json);
-      }
-    } catch (err) {
-      setError(err);
-    } finally {
-      setPending(false);
-    }
-  }, [body, headers, method, url]);
+//   const fetchHandler = useCallback(async () => {
+//     setPending(true);
+//     try {
+//       const res = await fetch(url, { 
+//         method,
+//         headers,
+//        });
+//       if(body) {
+//         const json = await res.json();
+//         setData(json);
+//       }
+//     } catch (err) {
+//       setError(err);
+//     } finally {
+//       setPending(false);
+//     }
+//   }, [body, headers, method, url]);
 
-  useEffect(() => {
-    fetchHandler();
-  }, [url, method, headers, body, fetchHandler]);
-  return { data, error, pending };
-}
+//   useEffect(() => {
+//     fetchHandler();
+//   }, [url, method, headers, body, fetchHandler]);
+//   return { data, error, pending };
+// }
